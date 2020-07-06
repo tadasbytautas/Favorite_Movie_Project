bbbd9dbbe3a883bc74fd1013db2f68cbb0492ac7
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-06 17:05:25 +0100

    added templates

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'My Awesome Movie Website, YAY!'
+    return render_template('homepage.html', title='Homepage')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
 
 
 if __name__ == '__main__':
