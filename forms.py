cb3a7ee8f23fba4e195e8efd3117dcb52c17411b
tadasbytautas <tadasbytautas1@gmail.com>
2020-07-07 14:51:57 +0100

    added form code

---
+++ forms.py
@@ -0,0 +1,39 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class PostForm(FlaskForm):
+    f_name = StringField(
+        'First Name',
+        validators=[
+            DataRequired(),
+            Length(min=1, max=30)
+        ]
+    )
+
+    l_name = StringField(
+        'Last Name',
+        validators=[
+            DataRequired(),
+            Length(min=1, max=30)
+        ]
+    )
+
+    title = StringField(
+        'Title',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=100)
+        ]
+    )
+
+    content = StringField(
+        'Content',
+        validators=[
+            DataRequired(),
+            Length(min=4, max=300)
+        ]
+    )
+
+    submit = StringField('Make a Post')
